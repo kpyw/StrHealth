9a241cdbc7ba8e958ba81fc9a415cd8ec7bc230b
kpy <kate@drkatewolin.com>
2013-11-06 18:43:05 -0600

    created workout model anew

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,47 @@
+class WorkoutsController < ApplicationController
+
+  def index
+    @workouts = Workout.all
+  end
+
+  def show
+    @workout = Workout.find_by(:id => params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @workout = Workout.new
+    @workout.wo_name = params[:wo_name]
+    @workout.exercise_id = params[:exercise_id]
+    
+    if @workout.save
+      redirect_to workouts_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @workout = Workout.find_by(:id => params[:id])
+  end
+
+  def update
+    @workout = Workout.find_by(:id => params[:id])
+    @workout.wo_name = params[:wo_name]
+    @workout.exercise_id = params[:exercise_id]
+    
+    if @workout.save
+      redirect_to workouts_url
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @workout = Workout.find_by(:id => params[:id])
+    @workout.destroy
+    redirect_to workouts_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 StrHealth::Application.routes.draw do
 
+  # Routes for the Workout resource:
+  # CREATE
+  get '/workouts/new', controller: 'workouts', action: 'new', :as => 'new_workout'
+  post '/workouts', controller: 'workouts', action: 'create'
+
+  # READ
+  get '/workouts', controller: 'workouts', action: 'index'
+  get '/workouts/:id', controller: 'workouts', action: 'show', :as => 'workout'
+
+  # UPDATE
+  get '/workouts/:id/edit', controller: 'workouts', action: 'edit', :as => 'edit_workout'
+  patch '/workouts/:id', controller: 'workouts', action: 'update'
+
+  # DELETE
+  delete '/workouts/:id', controller: 'workouts', action: 'destroy'
+  #------------------------------
+
 root 'exercises#index'
 
 get '/about' => 'pages#about'
